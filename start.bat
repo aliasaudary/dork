9803c7959ce54d72e30e29bed932d32d24649d28
aliasaudary <89685163+aliasaudary@users.noreply.github.com>
2021-08-28 16:33:09 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,7 +14,7 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Success!
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+tasklist | find /i "dork.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
 echo Password: XXXXXX
 echo You can login now.
